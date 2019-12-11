1a4a45cc704ce4ad0501329c79f40ae32d971272
Balaji Londhe <blondhe1@in.ibm.com>
2019-12-11 18:28:16 +0530

    Updated postman collection paths

--- cicd/pipelines/functions/newman.groovy
+++ cicd/pipelines/functions/newman.groovy
@@ -4,10 +4,6 @@ def runTest(testCollection, testEnvironment) {
    echo "Testing collection ${testCollection} against environment ${testEnvironment}"
    // TODO create a jenkins slave image with newman install
    // meanwhile, install it everytime...
-   sh "ls -ltr"
-   sh "cd cicd"
-   sh "ls -ltr"
-   sh "pwd"
    sh "npm install -g newman"
    sh "newman run ${testCollection} -e ${testEnvironment} --disable-unicode --color off"
 }
