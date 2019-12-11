dac3334bdd37f97f0ae728cadf49adc01b4dce7d
Balaji Londhe <blondhe1@in.ibm.com>
2019-12-11 18:06:13 +0530

    Update newman parameter to --no-color

--- cicd/pipelines/functions/newman.groovy
+++ cicd/pipelines/functions/newman.groovy
@@ -5,7 +5,7 @@ def runTest(testCollection, testEnvironment) {
    // TODO create a jenkins slave image with newman install
    // meanwhile, install it everytime...
    sh "npm install -g newman"
-   sh "newman run ${testCollection} -e ${testEnvironment} --no-color"
+   sh "newman run ${testCollection} -e ${testEnvironment} --disable-unicode --color off"
 }
 
 return this
