95550f8630089a75e61443bedb51bd00e35b6d95
Balaji Londhe <blondhe1@in.ibm.com>
2019-12-12 18:28:10 +0530

    Changes reverted

--- societyapp_run_pipelines.sh
+++ societyapp_run_pipelines.sh
@@ -5,11 +5,11 @@ oc start-build member-app-pipeline
 echo "Wait for 10 mins for this pipeline to finish so that other builds will be faster as nexus would have cached the maven artifacts by then"
 sleep 600
 oc start-build vehicle-app-pipeline
-sleep 60
-echo "Wait for 2 mins to finish the build"
+sleep 180
+echo "Wait for 3 mins to finish the build"
 oc start-build parking-app-pipeline
-echo "Wait for 2 mins to finish the build"
-sleep 120
+echo "Wait for 3 mins to finish the build"
+sleep 180
 oc start-build apartment-app-pipeline
-echo "Wait for 2 mins to finish the build"
-sleep 120
+echo "Wait for 3 mins to finish the build"
+sleep 180
