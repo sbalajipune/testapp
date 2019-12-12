60cdcb626992e83d0e8b609a91581806f9cb7931
Balaji Londhe <blondhe1@in.ibm.com>
2019-12-12 17:54:34 +0530

    Updated external docker registry

--- societyapp_run_pipelines.sh
+++ societyapp_run_pipelines.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+oc project societyapp-dev
 echo "Running the build and deployment pipelines"
 oc start-build member-app-pipeline
 echo "Wait for 10 mins for this pipeline to finish so that other builds will be faster as nexus would have cached the maven artifacts by then"
